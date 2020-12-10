cf1eb1186a98c5fbce273a6fa0881a9dcf6856a6
Mancone Alexis <ale.mancone@gmail.com>
2020-12-10 15:11:24 +0100

    menuMusic

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -2,14 +2,16 @@ package com.company;
 
 import java.util.Scanner;
 
+
 public class Menu {
-    public static void printError(){
+    public static void printError() {
         /**
          * When you write something wrong the function will send you the error message and allow you to start over
          */
         System.out.println("Not a valid command");
     }
-    public static char menu (char menu){
+
+    public static char menu(char menu){
         /**
          * retrieves information sent by the user
          */
@@ -22,13 +24,13 @@ public class Menu {
              */
             case 'm' -> {
                 System.out.println(
-                        "    \nWelcome\n"+
-                        "        You can choose between:\n"+
-                        "---------------------------------------------------------------\n"+
-                        "|     'play', 'p' : to play at Colossal Cave                  |\n"+
-                        "|     'song', 's' : to listen music                           |\n"+
-                        "|     'quit', 'q' : to quit the system                        |\n"+
-                        "---------------------------------------------------------------\n"
+                        "    \nWelcome\n" +
+                                "        You can choose between:\n" +
+                                "---------------------------------------------------------------\n" +
+                                "|  Press 'play', 'p' : to play at Colossal Cave               |\n" +
+                                "|  Press 'song', 's' : to listen music                        |\n" +
+                                "|  Press 'quit', 'q' : to quit the system                     |\n" +
+                                "---------------------------------------------------------------\n"
                 );
                 chooseMenu = sc.next();
                 switch (chooseMenu) {
@@ -43,7 +45,6 @@ public class Menu {
                 //call function "play" for colossal cave
 
 
-
                 System.out.println("Press 'r','return' to return to the menu or 'q','quit' to quit the program");
                 chooseMenu = sc.next();
                 switch (chooseMenu) {
@@ -55,14 +56,19 @@ public class Menu {
 
             case 's' -> {
 
-                Playlist playlist1 = new Playlist();
+                char menuMusic = 'M';
 
-                //call function "music" to listen music
-                System.out.println("");//menu for music to choose what you want
+                while ( menuMusic != 'm' ) {    //to quit the menu
+                    menuMusic = com.company.menuMusic.menuMusic(menuMusic);
+                }
+                return 'm';
+                    /*Playlist playlist1 = new Playlist();
 
+                    //call function "music" to listen music
+                    System.out.println("");//menu for music to choose what you want
 
-                System.out.println("Type in 'play' to start listening your playlist");
-                for (int i = 0; i < playlist1.playlist.size(); i++ ) {
+                    System.out.println("Type in 'play' to start listening your playlist");
+                    for (int i = 0; i < playlist1.playlist.size(); i++) {
 
                     Song song = playlist1.playlist.get(i);
                     String songName = song.getTitle();
@@ -80,31 +86,29 @@ public class Menu {
                             break;
                         }
                     }
-                }
 
 
-                Scanner musicSc = new Scanner(System.in);
+                    Scanner musicSc = new Scanner(System.in);
 
-                System.out.println("Type in title, artist and duration");
-                String title = musicSc.nextLine();
-                String artist = musicSc.nextLine();
-                String duration = musicSc.nextLine();
-
-
-                playlist1.addSong(title, artist, duration);
-                System.out.println(playlist1.playlist);
+                    System.out.println("Type in title, artist and duration");
+                    String title = musicSc.nextLine();
+                    String artist = musicSc.nextLine();
+                    String duration = musicSc.nextLine();
 
 
-                System.out.println("Press 'r','return' to return to the menu or 'q','quit' to quit the program");
-                chooseMenu = sc.next();
-                switch (chooseMenu) {
-                    case "r", "return", "exit" -> menu = 'm';
-                    case "q", "quit" -> menu = 'q';
-                    default -> printError();
-                }
+                    playlist1.addSong(title, artist, duration);
+                    System.out.println(playlist1.playlist);
+
+                    System.out.println("Press 'r','return' to return to the menu or 'q','quit' to quit the program");
+                    chooseMenu = sc.next();
+                    switch (chooseMenu) {
+                        case "r", "return", "exit" -> menu = 'm';
+                        case "q", "quit" -> menu = 'q';
+                        default -> printError();
+                    }*/
             }
 
-        }
+            }
         return menu;
     }
 }
---
+++ src/com/company/menuMusic.java
@@ -0,0 +1,57 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class menuMusic {
+    public static void printError(){
+        /**
+         * When you write something wrong the function will send you the error message and allow you to start over
+         */
+        System.out.println("Not a valid command");
+    }
+    public static char menuMusic (char menuMusic){
+        Scanner menuMusicSc = new Scanner(System.in);
+        String chooseMenuMusic;
+        switch (menuMusic){
+            case 'M' -> {
+                System.out.println(
+                        "    \nWelcome\n"+
+                        "        You can choose between:\n"+
+                        "---------------------------------------------------------------\n"+
+                        "|Press 'playlist','p' to add or remove a song to your playlist|\n"+
+                        "|Press 'listen','l' to listen your playlist                   |\n"+
+                        "|Press 'quit','q' to return at the menu                       |\n"+
+                        "---------------------------------------------------------------\n"
+                );
+                chooseMenuMusic = menuMusicSc.next();
+                switch (chooseMenuMusic) {
+                    case "playlist", "p" -> menuMusic = 'p';
+                    case "listen", "l" -> menuMusic = 'l';
+                    case "quit", "q" -> menuMusic = 'm';
+                    default -> printError();
+                }
+            }
+            case'p'->{
+                System.out.println("add/remove song");
+
+                System.out.println("Press 'r','return' to return to the menu");
+                chooseMenuMusic = menuMusicSc.next();
+                switch (chooseMenuMusic) {
+                    case "return", "r" -> menuMusic = 'M';
+                    default -> printError();
+                }
+            }
+            case'l'->{
+                System.out.println("listen");
+
+                System.out.println("Press 'r','return' to return to the menu");
+                chooseMenuMusic = menuMusicSc.next();
+                switch (chooseMenuMusic) {
+                    case "return", "r" -> menuMusic = 'M';
+                    default -> printError();
+                }
+            }
+        }
+        return menuMusic;
+    }
+}
